5ae0b88f94e8dafcf2ab2d888b62809d015cc043
abdelmun3m <abdelmunemelbasiouny@gmail.com>
2017-12-29 16:55:59 +0200

    add 3.3

---
+++ client/Tic-Tac-Toe-Zoo-client-v0.3.3/src/tic/tac/toe/zoo/client/v0/pkg0/pkg1/Player.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tic.tac.toe.zoo.client.v0.pkg0.pkg1;
+
+import java.util.List;
+
+/**
+ *
+ * @author abdelmun3m
+ */
+public class Player {
+    String userName ;
+    String passWord ;
+    List requestList ;
+    List gameList ;
+    
+    
+    
+}
